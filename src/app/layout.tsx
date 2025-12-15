34d92f4d6de447b0c3747a184927145e8fce4cf7
Akshay <akshay@Akshays-MacBook-Air.local>
2025-12-15 22:31:05 +0530

    reverted 90e9ea2aa44c98ba7c60ad96e50165879c199f51

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/auth-context";
-import { ErrorReporter } from "@/components/shared/error-reporter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <ErrorReporter />
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
