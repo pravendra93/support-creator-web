7f5aaa92d691cc8a833e1f4cc7f9189bd42bc545
Pravendra Kumar <prvndrkumar55@gmail.com>
2026-02-22 11:01:20 +0530

    making changes for permance

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   output: "standalone",
+  experimental: {
+    optimizePackageImports: ["lucide-react", "@radix-ui/react-icons", "date-fns"],
+  },
 };
 
 export default nextConfig;
